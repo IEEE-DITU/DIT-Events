626aef3ce0b88e5976216ed308ec70ffe4edba2c
Mudit jain <muditj214@gmail.com>
2024-05-30 19:58:47 +0530

    setting branch

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,20 @@ import 'package:dit_events/screens/splash_screen.dart';
 import 'package:dit_events/utils/widget_extension.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main()
+{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return const MaterialApp(
-      home: SplashScreen(),
+      home: SplashScreen(  ),
     );
   }
 }
