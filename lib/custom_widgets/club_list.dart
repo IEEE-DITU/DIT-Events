268ddb577edca80f44451365f7df0093cc7343aa
Paras1089 <parassehgal2003@gmail.com>
2024-06-02 21:16:32 +0530

    ClubList

--- lib/custom_widgets/club_list.dart
+++ lib/custom_widgets/club_list.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter/material.dart';
 
@@ -9,64 +10,44 @@ class ClubList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Row(
-          children: [
-            Title(color: Colors.black, child: Text(categoryName)),
-          ],
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: 100,
-          child:  ListView.builder(
-          shrinkWrap: true,
-          physics: BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemCount: imageList.length,
-          itemBuilder: (context, index) {
-            return Image.asset(imageList[index], width: 100, height: 100,);
-          },
-        ),
-        )
-        //   children: [Title(color: Colors.black, child: Text('SPORTS'))],
-        // ),
-        // Row(
-        //   children: [
-        //     Card(
-        //       margin: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
-        //       color: Colors.grey,
-        //       child: Image.network(
-        //           'https://img.freepik.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg',
-        //           height: 100.0,
-        //           width: 100.0,
-        //           fit: BoxFit.cover),
-        //       //Padding(
-        //       // padding: const EdgeInsets.all(50.0),
-        //       // ),
-        //     ),
-        //     Card(
-        //         color: Colors.grey,
-        //         child: Column(
-        //           crossAxisAlignment: CrossAxisAlignment.center,
-        //           mainAxisAlignment: MainAxisAlignment.center,
-        //           children: [
-        //             Padding(
-        //                 padding:
-        //                     const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0)),
-        //             Image.network(
-        //               'https://img.freepik.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg',
-        //               height: 100.0,
-        //               width: 100.0,
-        //               fit: BoxFit.cover,
-        //             ),
-        //           ],
-        //         )),
-        //   ],
-        // ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 10),
+      child: Column(
+
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          //Padding(padding:const EdgeInsets.fromLTRB(100, 0, 100, 10)),
+          Row(
+            children: [
+              Title(color: Colors.black, child:
+              Text(categoryName,style:GoogleFonts.inriaSerif(fontWeight:FontWeight.w400,fontSize:18 ),)),
+            ],
+          ),
+
+      SizedBox(
+              width: 400,
+              height: 90,
+              child:  ListView.builder(
+                shrinkWrap: true,
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: imageList.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(onTap:  (){}, child:
+                    Card(
+                        color: Colors.white,
+                      child:SizedBox(
+                        height:110,
+                        width: 110,
+                        child:
+                    Image.asset(imageList[index], width: 100, height: 100,),
+                      ),),);
+                    },
+              ),
+      ),
+        ],
+      ),
     );
     Text("HELLo World");
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dit_events/custom_widgets/club_list.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,12 +15,35 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body:
-        Column(
-          children: [
-            ClubList(categoryName: 'SPORTS', imageList: ['Assets/photo1.png', 'Assets/photo1.png'],),
-            ClubList(categoryName: 'MUSIC', imageList: ['Assets/photo1.png']),
-            ClubList(categoryName: 'ARTS', imageList: ['Assets/photo1.png', 'Assets/photo1.png', 'Assets/photo1.png', 'Assets/photo1.png']),
-          ],
+        SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 10),
+                child: Text('FEATURED',style:GoogleFonts.inriaSerif(fontWeight:FontWeight.w400,fontSize:25),),
+              ),
+              GestureDetector( onTap: (){},
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(60, 0, 80, 30),
+                  child: Image.asset('Assets/photo1.png'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 10),
+                child: Text('UPCOMING EVENTS',style:GoogleFonts.inriaSerif(fontWeight:FontWeight.w400,fontSize:25),),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 10),
+                child: Text('CLUBS & SOCIETIES',style:GoogleFonts.inriaSerif(fontWeight:FontWeight.w400,fontSize:25),),
+              ),
+              ClubList(categoryName: 'SPORTS', imageList: ['Assets/photo1.png', 'Assets/photo2.jpg','Assets/photos7.jpg'],),
+              ClubList(categoryName: 'MUSIC', imageList: ['Assets/download.jpeg','Assets/photo1.png','Assets/photo2.jpg','Assets/photos6.png','Assets/download.jpeg',]),
+              ClubList(categoryName: 'ARTS', imageList: ['Assets/photos5.jpg','Assets/photo1.png', 'Assets/photos6.png','Assets/photos7.jpg']),
+            ],
+          ),
         )
     );
   }
