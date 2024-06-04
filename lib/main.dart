0999622cc82a32f40d334ac7849b67b3a2e18d0e
garima <garimagrawal5113@gmail.com>
2024-06-04 21:31:55 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dit_events/screens/naa_screen.dart';
 import 'package:dit_events/screens/splash_screen.dart';
 import 'package:dit_events/utils/widget_extension.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreen(),
+      home: NaaScreen(),
     );
   }
 }
--- lib/screens/naa_screen.dart
+++ lib/screens/naa_screen.dart
@@ -1,31 +1,62 @@
 
+
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
-class Naa_screen extends StatefulWidget {
-  const Naa_screen({super.key});
+
+class NaaScreen extends StatefulWidget {
+  const NaaScreen({super.key});
   static const routeName = '/naa-screen';
   @override
-  State<Naa_screen> createState() => _naa_screenState();
+  State<NaaScreen> createState() => _NaaScreenState();
 }
 
-class _naa_screenState extends State<Naa_screen> {
+class _NaaScreenState extends State<NaaScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
 
-        child: Column(
+        child:  Column(
 
-            mainAxisAlignment: MainAxisAlignment.center,
+
 
             children: [
-              Container(
-                  child: Text("NEWS AND ALERTS")
+              Padding(
+                  padding: EdgeInsets.only(top:70),
+                  child:
+                  Text('NEWS & ALERTS', style: GoogleFonts.inter( fontSize:35, fontWeight: FontWeight.w400)),),
+              Padding(
+                padding: EdgeInsets.only(left: 340),
+                child: Icon(
+                   Icons.filter_list,
+                   color: Colors.grey,
+                   size: 20.0),
               ),
-            ]
+              Row(
+                children: [
+                  SizedBox(
+                    height: 17, width: 60,
+                    child:
+                    Image(image: AssetImage('Assets/Ellipse 157.png')))
+                  ,
+                  SizedBox(width: 10),
+                  Column(
+                   children: [
+
+                    Padding(
+                        padding: EdgeInsets.only(top:17),
+                        child :Text('date', style: GoogleFonts.inriaSerif(fontSize: 30, fontWeight: FontWeight.w400))),
+                         Text('ABOUT', style: GoogleFonts.inriaSerif(fontSize: 20, fontWeight: FontWeight.w400))])
+                    ]
+                  ),
 
 
-        ),
+                ],
+
+              )
+
+
       ),
     );
   }
