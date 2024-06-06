0ef02e64ea47ca862773f863ca8083752cf79f55
TheDarkLord100 <miracleyt404@gmail.com>
2024-06-06 20:36:34 +0530

    Optimised and cleaned code

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: NaaScreen(),
+      home: NewsScreen(),
     );
   }
 }
-
-
--- lib/screens/naa_screen.dart
+++ lib/screens/naa_screen.dart
@@ -1,66 +1,48 @@
-
-
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
-
-class NaaScreen extends StatefulWidget {
-  const NaaScreen({super.key});
-  static const routeName = '/naa-screen';
+class NewsScreen extends StatefulWidget {
+  const NewsScreen({super.key});
   @override
-  State<NaaScreen> createState() => _NaaScreenState();
+  State<NewsScreen> createState() => _NewsScreenState();
 }
 
-class _NaaScreenState extends State<NaaScreen> {
+class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-
-        child:  Column(
-
-
-
-            children: [
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 70),
+            child: Text('NEWS & ALERTS',
+                style: GoogleFonts.inter(
+                    fontSize: 35, fontWeight: FontWeight.w400)),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 340),
+            child: Icon(Icons.filter_list, color: Colors.grey, size: 20.0),
+          ),
+          Row(children: [
+            const SizedBox(
+                height: 17,
+                width: 60,
+                child: Image(image: AssetImage('Assets/Ellipse 157.png'))),
+            const SizedBox(width: 10),
+            Column(children: [
               Padding(
-                  padding: EdgeInsets.only(top:70),
-                  child:
-                  Text('NEWS & ALERTS', style: GoogleFonts.inter( fontSize:35, fontWeight: FontWeight.w400)),),
-              Padding(
-                padding: EdgeInsets.only(left: 340),
-                child: Icon(
-                   Icons.filter_list,
-                   color: Colors.grey,
-                   size: 20.0),
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    height: 17, width: 60,
-                    child:
-                    Image(image: AssetImage('Assets/Ellipse 157.png')))
-                  ,
-                  SizedBox(width: 10),
-                  Column(
-                   children: [
-
-                    Padding(
-                        padding: EdgeInsets.only(top:17),
-                        child :Text('date', style: GoogleFonts.inriaSerif(fontSize: 30, fontWeight: FontWeight.w400))),
-                         Text('ABOUT', style: GoogleFonts.inriaSerif(fontSize: 20, fontWeight: FontWeight.w400))])
-                    ]
-                  ),
-
-
-                ],
-
-              )
-
-
-      ),
+                  padding: const EdgeInsets.only(top: 17),
+                  child: Text('date',
+                      style: GoogleFonts.inriaSerif(
+                          fontSize: 30, fontWeight: FontWeight.w400))),
+              Text('ABOUT',
+                  style: GoogleFonts.inriaSerif(
+                      fontSize: 20, fontWeight: FontWeight.w400))
+            ])
+          ]),
+        ],
+      )),
     );
   }
-
 }
-
-
