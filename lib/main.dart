1c721c2079b5edd12dc2553fa23d3f99d8313618
garima <garimagrawal5113@gmail.com>
2024-06-10 23:04:03 +0530

    side panel

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dit_events/screens/naa_screen.dart';
 import 'package:dit_events/screens/splash_screen.dart';
 import 'package:dit_events/utils/widget_extension.dart';
+import 'package:dit_events/screens/sidepanel_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NaaScreen(),
+      home: SidePanel_Screen(),
     );
   }
 }
